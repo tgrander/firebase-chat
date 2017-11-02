15cc43bed2e94e8bbf04fbc1b207d8043aabc2cd
tgrander <treygranderson@gmail.com>
2017-11-02 16:30:12 -0400

    display signin modal when navbar links are clicked

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,26 +1,50 @@
 import propTypes from 'prop-types';
 import React from 'react';
+import { Modal, Input } from 'antd';
 
 import './Navbar.css';
 
 
-function Navbar({ auth }) {
-  return (
-    <div className="navbar">
-      {
-            auth
-                ? (
-                  <a href="#">Logout</a>
-                )
-                : (
-                  <div>
-                    <a href="#">Signin</a>
-                    <a href="#">Signup</a>
-                  </div>
-                )
-        }
-    </div>
-  );
+class Navbar extends React.Component {
+
+    state = {
+        showModal: false
+    }
+
+    renderModal = () => {
+        this.setState({ showModal: !this.state.showModal })
+    }
+
+  render() {
+    const { props } = this;
+
+    return (
+      <div className="navbar">
+        {
+                  props.auth
+                      ? (
+                        <a href="#">Logout</a>
+                      )
+                      : (
+                        <div className="nav-links">
+                          <a href="#" onClick={this.renderModal}>Sign In</a>
+                          <a href="#" onClick={this.renderModal}>Create Account</a>
+                        </div>
+                      )
+              }
+
+        <Modal
+          title="Basic Modal"
+          visible={this.state.showModal}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+        >
+          <Input placeholder="Email" />
+          <Input placeholder="Password" />
+        </Modal>
+      </div>
+    );
+  }
 }
 
 Navbar.propTypes = {
