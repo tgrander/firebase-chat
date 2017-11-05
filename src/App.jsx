639b4c8ff2e7960aee948ca676e9823315af3bb0
tgrander <treygranderson@gmail.com>
2017-11-05 18:34:21 -0500

    integrate react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom';
 import './App.css';
-import ChangeColor from './components/ChangeColor';
 import Landing from './components/Landing';
-import MessagesContainer from './components/MessagesContainer';
+import Messages from './components/MessagesContainer';
 import Navbar from './components/Layout/Navbar';
-import TextInputContainer from './components/TextInputContainer';
 import store from './store';
 
 
 function App() {
   return (
-    <Provider store={store}>
-      <div className="App">
-        <Navbar />
-        <div className="content">
-          <Landing />
+    <Router>
+      <Provider store={store}>
+        <div className="App">
+          <Navbar />
+          <div className="content">
+            <Route exact path="/" component={Landing} />
+            <Route path="/messages" component={Messages} />
+          </div>
         </div>
-      </div>
-    </Provider>
+      </Provider>
+    </Router>
   );
 }
 
