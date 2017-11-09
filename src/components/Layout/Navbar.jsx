07afe0e3b70d12edc3fbd0baa4b33bc77badadbf
tgrander <treygranderson@gmail.com>
2017-11-09 14:01:23 -0500

    cleanup Navbar

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,39 +1,16 @@
 import propTypes from 'prop-types';
 import React from 'react';
-import { Modal, Icon, Input } from 'antd';
-import { auth } from '../../firebase'
+import { Icon } from 'antd';
+import { auth } from '../../firebase';
 
 import './Navbar.css';
 
 
 class Navbar extends React.Component {
-
-    state = {
-        showModal: false,
-        email: '',
-        password: ''
-    }
-
-    renderModal = () => {
-
-        this.setState({ showModal: !this.state.showModal })
-    }
-
-    handleOk = () => {
-
-        const { email, password } = this.state
-
-        // auth.signInWithEmailAndPassword(email, password)
-        //     .then(() => this.renderModal())
-        //     .catch(err => console.error(err))
-
-        this.setState({ email: '', password: '' })
-    }
-
-    onClickLogout = () => {
-        auth.signOut()
-            .then(() => this.props.signOut())
-    }
+  onClickLogout = () => {
+    auth.signOut()
+      .then(() => this.props.signOut());
+  }
 
   render() {
     const { props } = this;
@@ -41,32 +18,22 @@ class Navbar extends React.Component {
     return (
       <div className="navbar">
 
-          <div className="nav-container">
-              <div className="logo">
-                  <div>Chat It Up</div>
-                  <Icon type="message" />
-              </div>
+        <div className="nav-container">
+          <div className="logo">
+            <div>Chat It Up</div>
+            <Icon type="message" />
+          </div>
 
-             {
+          {
                props.isAuthorized
-                   ?  <a href="#" onClick={this.onClickLogout}>Logout</a>
+                   ? <a href="#" onClick={this.onClickLogout}>Logout</a>
                    : (
                      <div className="nav-links">
                        <a href="#" onClick={this.renderModal}>Sign In</a>
                      </div>
                    )
              }
-          </div>
-
-        <Modal
-          title="Basic Modal"
-          visible={this.state.showModal}
-          onOk={this.handleOk}
-          onCancel={this.renderModal}
-        >
-          <Input placeholder="Email" onChange={e => this.setState({ email: e.target.value })} />
-          <Input placeholder="Password" onChange={e => this.setState({ password: e.target.value })} />
-        </Modal>
+        </div>
       </div>
     );
   }
